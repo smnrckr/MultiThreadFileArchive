6858ab3a7e3e28a82a883116dbeb3eb1e36d2756
elifkeles <elifkeless2003@gmail.com>
2025-07-16 20:25:42 +0300

    Zip/Unzip ve zip sonrası dosya silme işlemleri yapıldı.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-import thread.FileArchiverZip_UnZip;
+import thread.FileArchiverUnzip;
+import thread.FileArchiverZip;
 import thread.FileProcessorThread;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.Semaphore;
 
@@ -19,13 +21,21 @@ public class Main {
                 fileProcessorThread.start();
             }
             try {
-                FileArchiverZip_UnZip zipThread = new FileArchiverZip_UnZip (
+                FileArchiverZip zipThread = new FileArchiverZip(
                         Paths.get("input"),
                         Paths.get("output/files.zip"),
-                        semaphore
+                        semaphore,
+                        true
                 );
                 zipThread.start();
                 zipThread.join();
+                FileArchiverUnzip unzipThread = new FileArchiverUnzip(
+                        Paths.get("output/files.zip"),
+                        Paths.get("unzipped_output"),
+                        semaphore
+                );
+                unzipThread.start();
+                unzipThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/thread/FileArchiverZip.java
+++ src/thread/FileArchiverZip.java
@@ -10,15 +10,18 @@ import java.util.concurrent.Semaphore;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public class FileArchiverZip_UnZip  extends Thread {
+public class FileArchiverZip  extends Thread {
     private final Path inputDirectory;
     private final Path outputZip;
     private final Semaphore semaphore;
+    private final boolean deleteAfterZip;
+    public static final int BUFFER_SIZE = 4096;
 
-    public FileArchiverZip_UnZip(Path inputDirectory, Path outputZip, Semaphore semaphore) {
+    public FileArchiverZip(Path inputDirectory, Path outputZip, Semaphore semaphore, boolean deleteAfterZip) {
         this.inputDirectory = inputDirectory;
         this.outputZip = outputZip;
         this.semaphore = semaphore;
+        this.deleteAfterZip=deleteAfterZip;
     }
     @Override
     public void run() {
@@ -26,20 +29,20 @@ public class FileArchiverZip_UnZip  extends Thread {
             semaphore.acquire();
             int currentCount = ThreadMonitor.activeThreads.incrementAndGet();
             System.out.println("[" + getName() + "] Zip thread started | Active threads: " + currentCount + "/10");
-            createOutputZipIfNotExist();
-            zipFiles();
+            List<Path> file_list = zipFiles();
+            if (deleteAfterZip) {
+                deleteFileFromZip(file_list);
+            }
             System.out.println("[" + getName() + "] Zip completed -> " + outputZip);
         } catch (IOException  | InterruptedException e) {
             System.err.println("[" + getName() + "] Error while zipping: " + e.getMessage());
             e.printStackTrace();
         }
     }
-    private void createOutputZipIfNotExist() throws IOException {
+    private List<Path> zipFiles() throws IOException {
         if (Files.notExists(outputZip.getParent())) {
             Files.createDirectories(outputZip.getParent());
         }
-    }
-    private void zipFiles() throws IOException {
         List<Path> files = Files.list(inputDirectory)
                 .filter(path -> path.toString().endsWith(".txt"))
                 .toList();
@@ -48,18 +51,29 @@ public class FileArchiverZip_UnZip  extends Thread {
                 addFileToZip(file, zipOut);
             }
         }
+        return files;
     }
     private void addFileToZip(Path file, ZipOutputStream zipOut) throws IOException {
         try(FileInputStream fileIn = new FileInputStream(file.toFile())) {
             ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
             zipOut.putNextEntry(zipEntry);
-            byte[] bytes = new byte[1024];
+            byte[] bytes = new byte[BUFFER_SIZE];
             int length;
             while ((length = fileIn.read(bytes)) != -1) {
                 zipOut.write(bytes, 0, length);
             }
             zipOut.closeEntry();
             System.out.println("[" + getName() + "] Zipped: " + file.getFileName());
+        }
+    }
+    private void deleteFileFromZip(List<Path> files) throws IOException {
+        for (Path file : files) {
+            try {
+                Files.deleteIfExists(file);
+                System.out.println("[" + getName() + "] Deleted: " + file);
+            } catch (IOException e) {
+                System.err.println("[" + getName() + "] Could not delete: " + file.getFileName() + " (" + e.getMessage() + ")");
+            }
         }
     }
 }
