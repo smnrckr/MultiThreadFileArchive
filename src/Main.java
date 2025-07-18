87a3b0bf545b9570c3de51ab17be47c5b20a61ea
Omer <omrgnts61@gmail.com>
2025-07-18 17:51:15 +0300

    ResultCollector eklendi, worker ve main entegre edildi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import thread.FileProcessorThread;
+import service.ResultCollector;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
 public class Main {
@@ -11,11 +14,24 @@ public class Main {
 
         if (txtFiles != null) {
             Semaphore semaphore = new Semaphore(10);
+            List<Thread> threads = new ArrayList<>();
 
             for (File file : txtFiles) {
                 FileProcessorThread fileProcessorThread = new FileProcessorThread(file, semaphore);
+                threads.add(fileProcessorThread);
                 fileProcessorThread.start();
             }
+
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            ResultCollector.printAllResults();
+
         } else {
             System.out.println("No files found in input folder.");
         }
